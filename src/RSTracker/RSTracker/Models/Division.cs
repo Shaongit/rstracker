b34be54a665593e43a66f0bd06b8ea7631d363ba
mahedee <mahedee.hasan@gmail.com>
2018-02-15 18:16:14 +0600

    modified division info

--- src/RSTracker/RSTracker/Models/Division.cs
+++ src/RSTracker/RSTracker/Models/Division.cs
@@ -9,11 +9,26 @@ namespace RSTracker.Models
 {
     public class Division
     {
+        public Division()
+        {
+            CreateDate = DateTime.Now;
+            ModifiedDate = DateTime.Now;
+            CreatedBy = null;
+            ModifiedBy = null;
+        }
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set;  }
-        [Display(Name ="Division")]
+        [Display(Name ="Division Name")]
         public string Name { get; set; }
 
         public virtual List<Employee> Employees { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        //User Id
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
     }
 }
